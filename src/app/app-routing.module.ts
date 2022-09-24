f223ce42a54e224d027fcc2ce7ff0a5792c0976f
CristianRiveraEsparza <cristianriveraesparza890@gmail.com>
2022-09-24 10:35:50 -0500

    Clase 24 de septiembre 2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SumaComponent } from './calculos/suma/suma.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+
+const routes: Routes = [
+  {path:'', component: ProductListComponent},
+  {path:'suma', component: SumaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
